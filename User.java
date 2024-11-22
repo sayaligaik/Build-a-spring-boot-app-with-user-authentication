828892394060e300933b7883cbdd66221fe1bb41
Sayali Gaikwad <124872737+sayaligaik@users.noreply.github.com>
2024-11-22 21:13:10 +0530

    User.java

---
+++ User.java
@@ -0,0 +1,20 @@
+package com.example.authapp.model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Entity
+@Data
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true, nullable = false)
+    private String username;
+
+    @Column(nullable = false)
+    private String password;
+
+    private String role = "ROLE_USER";
+}
