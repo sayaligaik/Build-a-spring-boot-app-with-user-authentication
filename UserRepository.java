8fbfd77373270abfeef459a0a573cc29ce3a7d50
Sayali Gaikwad <124872737+sayaligaik@users.noreply.github.com>
2024-11-22 21:13:37 +0530

    Create UserRepository.java

---
+++ UserRepository.java
@@ -0,0 +1,8 @@
+package com.example.authapp.repository;
+
+import com.example.authapp.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    User findByUsername(String username);
+}
